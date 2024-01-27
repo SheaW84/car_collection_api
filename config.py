99bec4865f324d561446be6cb881a510b6ac6426
SheaW84 <sheawilsn@gmail.com>
2024-01-26 20:13:09 -0600

    filled out config

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-FLASK_
+import os
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.dirpath(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
+    FLASK_APP = os.getenv('FLASK_APP')
+    # FLASK_DEBUG = os.getenv('FLASK_DEBUG')
+    FLASK_ENV = os.getenv('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Something else to say'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
